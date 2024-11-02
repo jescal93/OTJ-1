bfe31dede78343d4b727f64e3cb2c56259b66a65
Jason Escalera <jasonescalera93@gmail.com>
2024-11-01 20:54:25 -0700

    added js code

---
+++ PawsClawsPetStore/index.js
@@ -0,0 +1,24 @@
+
+const storeName = "Paws & Claws";
+const storeType = "Pet Store";
+const storeHours = "Monday - Friday: 9 AM - 6 PM, Saturday: 10 AM - 4 PM, Sunday: Closed";
+
+const product1 = "Dog Food";
+const product2 = "Dog Beds";
+const product3 = "Dog Shampoo";
+const product4 = "Dog Toys";
+const product5 = "Dog Kennels";
+
+const price1 = 29.99;
+const price2 = 25.99;
+const price3 = 12.99;
+const price4 = 10.99;
+const price5 = 50.00;
+
+console.log(storeName, storeType);
+console.log(storeHours);
+console.log(product1, price1);
+console.log(product2, price2);
+console.log(product3, price3);
+console.log(product4, price4);
+console.log(product5, price5);
